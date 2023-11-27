919b0badc68e48f4b856b0b9fb4ac379f0cc9098
BT_2N2_25 <bths@sample.com>
2023-11-27 13:54:46 -0500

    added stuff in oregon logic

--- src/OregonLogic.java
+++ src/OregonLogic.java
@@ -1,11 +1,21 @@
 public class OregonLogic {
 
-    public OregonLogic() { }
+    private int water;
+    private int food;
+
+    public OregonLogic() {
+        water = 0;
+        food = 0;
+
+    }
 
     //returns a random integer between a and b, b is not inclusive
     public int randInt(int a, int b) {
         return (int) (Math.random() * (b - a)) + a;
     }
 
+    public boolean isDead() {
+        return (water <= 0 || food <= 0);
+    }
 
 }
